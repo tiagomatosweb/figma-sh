e92e752eadae848cdc48dedb5c37082bfde2732a
Luis Davi <luisdavi.contato@gmail.com>
2022-12-30 17:19:16 -0300

    fixed requests problems and align lines

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,39 @@
 <template>
     <div class="p-4">
         <div class="grid grid-cols-4 gap-2 mb-4">
-            <select name="" class="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
+            <div class="w-screen h-screen fixed top-0 left-0 z-40 bg-white flex items-center justify-center"
+                v-if="loadScreen">
+                <span>Carregando...</span>
+            </div>
+            <select name=""
+                class="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
                 <option value="">Monakai</option>
                 <option value="">Material</option>
             </select>
 
-            <select name="" id="" class="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
+            <select name="" id=""
+                class="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base  focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
                 <option value="">Javascript</option>
                 <option value="">PHP</option>
             </select>
-            
-            <button class="p-2 bg-blue-500 text-white font-mono font-bold h-max w-max transition-all rounded hover:ring focus:bg-blue-600">
-                Run
-            </button>
+
+            <div class="flex flex-row gap-2">
+                <button
+                    class="p-2 bg-blue-500 text-white font-mono font-bold h-max w-max transition-all rounded hover:ring focus:bg-blue-600">
+                    Run
+                </button>
+
+                <button
+                    class="p-2 bg-white border border-black text-black font-mono font-bold h-max w-max transition-all rounded ring-gray-300 hover:ring focus:bg-black focus:text-white">
+                    Copy
+                </button>
+
+            </div>
 
         </div>
 
-        <div id="editor" class="rounded-lg py-4 pb-8 min-h-[500px]">
-            <CodeEditor />
+        <div class="rounded-lg py-4 pb-8 h-[500px] overflow-scroll" :style="`background-color: ${backgroundColor}`">
+            <CodeEditor @set-load="loadScreen = false" @set-background="setBackground" />
         </div>
 
         <CreateRectangle />
@@ -26,16 +41,14 @@
 </template>
 
 <script setup>
-import { ref} from 'vue'
+import { ref } from 'vue'
 import CodeEditor from './components/CodeEditor.vue';
 import CreateRectangle from './components/CreateRectangle.vue'
-
-const currentHtmlCode= ref("")
-const textCodeValue = ref('')
-
+const loadScreen = ref(true)
+const backgroundColor = ref('')
+function setBackground(payload){
+    backgroundColor.value = payload
+}
 </script>
 <style>
-#editor {
-    background-color: #2e3440;
-}
 </style>
--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -1,42 +1,69 @@
 <template>
-    <div class="flex flex-row gap-2 px-2">
-        <div class="flex flex-col items-end p-1 border-r-2 border-gray-800">
-            <span class="text-gray-700 font-mono" v-for="line in codeLines" v-bind:key="line">{{ line }}</span>
+    <div class="flex flex-row gap-2 px-2 min-h-full">
+        <div class="flex flex-col items-end px-2" id="countDisplay">
+            <span class="text-gray-500 font-mono flex items-center justify-center" id="count" v-for="line in codeLines"
+                v-bind:key="line">{{ line }}</span>
         </div>
-        <div class="relative w-full h-max">
-            <div class="break-all text-white" v-html="htmlCode">
-            </div>
-            <textarea class="absolute right-0 z-20 top-0 w-full min-h-[500px] mb-4 p-0 bg-transparent text-transparent"
-                placeholder="Seu código aqui..." v-model="textCodeValue" name="code"></textarea>
+        <div class="relative w-full min-h-full">
+            <textarea id="teste"
+                class="absolute z-20 focus:outline-0 focus:ring-0 w-full min-h-full caret-white pt-[5px] bg-transparent text-transparent overflow-hidden"
+                placeholder="Seu código aqui..." spellcheck="false" v-model="textCodeValue" name="code"></textarea>
+
+            <div class="break-all text-white" v-html="htmlCode"></div>
         </div>
     </div>
 </template>
 
 <script setup>
-import { ref, watch } from "vue";
+import { ref, watch, onMounted, defineEmits } from "vue";
 import { getHighlighter } from 'shiki'
-const textCodeValue = ref('')
-const codeLines = ref(1) // count number example
+const textCodeValue = ref('TESTE')
+const codeLines = ref(1)
 const htmlCode = ref('')
-watch(textCodeValue, () => {
-    codeLines.value = textCodeValue.value.split('\n').length;
+const setTheme = ref({})
+const emit = defineEmits(['set-load', 'set-background'])
+function loadTheme() {
     getHighlighter({
-      theme: 'nord'
+        theme: 'dracula',
+        langs: ['javascript', 'js']
     }).then(highlighter => {
-      const code = highlighter.codeToHtml(`${textCodeValue.value}`, { lang: 'js' })
-      htmlCode.value = code
+        console.log('executou')
+        setTheme.value = highlighter
+        emit('set-load')
+        emit('set-background', highlighter.getBackgroundColor('dracula'))
     })
-    
+}
+
+onMounted(async () => {
+    loadTheme()
+})
+
+watch(textCodeValue, async () => {
+    codeLines.value = textCodeValue.value.split('\n').length;
+    htmlCode.value = await setTheme.value.codeToHtml(`${textCodeValue.value}`, { lang: 'js' })
 })
 
 </script>
 
 <style>
-textarea {
-    outline: #00000000;
+#teste {
+    margin: 0;
+    padding: 0;
+    outline: unset;
     border: none;
     resize: none !important;
-    margin-bottom: -1rem;
+    font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+    font-size: 1em;
+    /* line-height: 27.3px; */
+}
+
+#count {
+    font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+    font-size: 1em;
+}
+
+#countDisplay {
+   
 }
 
 textarea:focus {
