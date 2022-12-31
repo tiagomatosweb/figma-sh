6ba42baf78f228c5dd70d2a6d48c249ddf3f1cef
Luis Davi <luisdavi.contato@gmail.com>
2022-12-31 15:16:43 -0300

    added dropdown component

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,49 @@
 <template>
   <div class="p-4">
-    <div class="grid grid-cols-4 gap-2 mb-4">
+    <div class="w-screen h-screen fixed top-0 left-0 z-40 bg-white flex items-center justify-center" v-if="loadScreen">
       <div
-        class="w-screen h-screen fixed top-0 left-0 z-40 bg-white flex items-center justify-center"
-        v-if="loadScreen"
-      >
-        <div
-          class="animate-spin inline-block w-10 h-10 border-[3px] border-current border-t-transparent text-black rounded-full"
-          role="status"
-          aria-label="loading"
-        >
-          <span class="sr-only">Loading...</span>
-        </div>
+        class="animate-spin inline-block w-10 h-10 border-[3px] border-current border-t-transparent text-black rounded-full"
+        role="status" aria-label="loading">
+        <span class="sr-only">Loading...</span>
       </div>
-      <select
-        class="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-      >
-        <option
-          v-for="(theme, index) in config.themes"
-          :key="index"
-          @click="setConfig('theme', theme)"
-        >
-          {{ theme }}
-        </option>
-      </select>
+    </div>
 
-      <select
-        class="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-      >
-        <option
-          v-for="(lang, index) in config.langs"
-          :key="index"
-          @click="setConfig('lang', lang.lang)"
-        >
-          {{ lang.title }}
-        </option>
-      </select>
+    <div class="grid grid-cols-4 gap-2 mb-4 items-end">
+      <div class="flex flex-col gap-2">
+        <span>Selecione o tema:</span>
+        <Dropdown :title="currentConfig.theme">
+          <span v-for="(theme, index) in config.themes" :key="index" @click="setConfig('theme', theme)"
+            class="cursor-pointer hover:bg-gray-900/75 px-2 py-1 rounded-md">
+            {{ theme }}
+          </span>
+        </Dropdown>
+      </div>
+
+      <div class="flex flex-col gap-2">
+        <span>Selecione o tema:</span>
+        <Dropdown :title="currentConfig.langTitle">
+          <span v-for="(lang, index) in config.langs" :key="index" @click="setConfig('lang', lang.lang); setConfig('langTitle', lang.title)"
+            class="cursor-pointer hover:bg-gray-900/75 px-2 py-1 rounded-md">
+            {{ lang.title }}
+          </span>
+        </Dropdown>
+      </div>
 
       <div class="flex flex-row gap-2">
         <button
-          class="p-2 bg-blue-500 text-white font-mono font-bold h-max w-max transition-all rounded hover:ring focus:bg-blue-600"
-        >
+          class="p-2 bg-blue-500 text-white font-mono font-bold h-max w-max transition-all rounded hover:ring focus:bg-blue-600">
           Run
         </button>
 
-        <button
-          @click="copyCode"
-          class="p-2 bg-white border border-black text-black font-mono font-bold h-max w-max transition-all rounded ring-gray-300 hover:ring focus:bg-black focus:text-white"
-        >
+        <button @click="copyCode"
+          class="p-2 bg-white border border-black text-black font-mono font-bold h-max w-max transition-all rounded ring-gray-300 hover:ring focus:bg-black focus:text-white">
           Copy
         </button>
       </div>
     </div>
 
-    <div
-      class="rounded-lg py-4 pb-8 h-[500px] overflow-scroll"
-      :style="`background-color: ${backgroundColor}`"
-    >
-      <CodeEditor
-        @set-load="loadScreen = false"
-        @set-background="setBackground"
-        :config="currentConfig"
-      />
+    <div class="rounded-lg py-4 pb-8 h-[500px] overflow-scroll" :style="`background-color: ${backgroundColor}`">
+      <CodeEditor @set-load="loadScreen = false" @set-background="setBackground" :config="currentConfig" />
     </div>
 
     <CreateRectangle />
@@ -72,13 +54,14 @@
 import { ref } from "vue";
 import CodeEditor from "./components/CodeEditor.vue";
 import CreateRectangle from "./components/CreateRectangle.vue";
+import Dropdown from "./components/Dropdown.vue";
 const currentConfig = ref({
   lang: "js",
-  theme: "nord",
+  langTitle: "Javascript",
+  theme: "Dracula",
 });
 const loadScreen = ref(true);
 const backgroundColor = ref("");
-
 const config = ref({
   themes: [
     "Dracula",
@@ -135,9 +118,15 @@ function copyCode() {
 function setConfig(key, value) {
   currentConfig.value[key] = value;
 }
+function selectedTheme(payload) {
+  setConfig("theme", payload);
+}
+function selectedLang(payload) {
+  setConfig("lang", payload);
+  setBackground;
+}
 
 function setBackground(payload) {
   backgroundColor.value = payload;
 }
 </script>
-<style></style>
---
+++ src/components/Dropdown.vue
@@ -0,0 +1,38 @@
+<template>
+    <div @click="checkModal = !checkModal"
+        :class="['dropdown relative h-max transition-all border-2 border-gray-200 rounded-md flex flex-col gap-4 cursor-pointer', {'border-indigo-500': checkModal}]">
+        <div class="text-black p-2 flex flex-row justify-between dropdown">
+            <span class="dropdown">
+                {{ title }}
+            </span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6 dropdown">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+            </svg>
+        </div>
+        <div v-show="checkModal"
+            class="w-full dropdown transition-all h-max backdrop-blur-[6px] mt-4 flex flex-col gap-2 absolute z-30 top-[35px] bg-gray-500/25 rounded-lg p-2 text-white border-2 border-gray-400">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps, ref, onMounted } from "vue";
+
+const props = defineProps({
+    title: String,
+});
+
+const checkModal = ref(false);
+
+
+onMounted(() => {
+    document.querySelector('body').addEventListener("click", (event)=>{
+        if(Array.from(event.target.classList).includes('dropdown')){
+            return 
+        }
+        checkModal.value = false
+    })
+})
+</script>
