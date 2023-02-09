c4a9d60b9302840bfee509b427c6b7732c1865a2
Tiago Matos <tiago@tiagomatos.com>
2023-02-09 17:23:06 +1100

    Add code mirror

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,27 @@
                 <div>
                     <label for="theme" class="block mb-1 text-xs font-semibold">Theme</label>
                     {{ theme }}
-                    <select
-                        id="theme"
-                        v-model="theme"
-                        class="block w-full rounded border-gray-300 py-2 pl-2.5 pr-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 text-xs"
-                    >
-                        <option value="dark">Dark</option>
-                        <option value="funky">Funky</option>
-                        <option value="atom-dark">atom dark</option>
-                        <option value="tomorrow">Tomorrow</option>
-                    </select>
+<!--                    <select-->
+<!--                        id="theme"-->
+<!--                        v-model="theme"-->
+<!--                        class="block w-full rounded border-gray-300 py-2 pl-2.5 pr-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 text-xs"-->
+<!--                    >-->
+<!--                        <option value="dark">Dark</option>-->
+<!--                        <option value="funky">Funky</option>-->
+<!--                        <option value="atom-dark">atom dark</option>-->
+<!--                        <option value="tomorrow">Tomorrow</option>-->
+<!--                    </select>-->
                 </div>
 
                 <div>
                     <label for="language" class="block mb-1 text-xs font-semibold">Language</label>
-                    <select
-                        id="language"
-                        v-model="currentLanguage"
-                        class="block w-full rounded-md border-gray-300 py-2 pl-2.5 pr-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 text-xs"
-                    >
-                        <option v-for="language in languages" :key="language" :value="language">{{ language }}</option>
-                    </select>
+<!--                    <select-->
+<!--                        id="language"-->
+<!--                        v-model="currentLanguage"-->
+<!--                        class="block w-full rounded-md border-gray-300 py-2 pl-2.5 pr-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 text-xs"-->
+<!--                    >-->
+<!--                        <option v-for="language in languages" :key="language" :value="language">{{ language }}</option>-->
+<!--                    </select>-->
                 </div>
             </div>
 
@@ -35,7 +35,7 @@
         </div>
 
         <div class="flex-1">
-            <AceEditor ref="aceEditor" />
+            <Editor ref="editorRef" />
         </div>
     </div>
 </template>
@@ -46,21 +46,22 @@ import { walkTree } from './utils/walkTree';
 import { calculateRGB } from './utils/calculateRGB';
 import { countNodeLength } from './utils/countNodeLength'
 import { usePrism } from './composables/usePrism';
-import AceEditor from './components/AceEditor.vue';
+import Editor from './components/Editor.vue';
 
 const { languages, currentLanguage, theme } = usePrism();
-const aceEditor = ref(null)
+const editorRef = ref(null)
 
 function buildPayloadMessage() {
-    const el = document.getElementsByClassName('ace_text-layer')[0]
+    const el = document.getElementsByClassName('cm-content')[0]
     const nodes = Array.from(el.childNodes)
+    console.log(nodes);
 
     // Build node range
     let output = []
 
     // Need to start negative as ace editor doesn't use <div> to break lines instead of \n.
     // So, it needs to count the line when the element with ace_line class arrives to the loop
-    let pointer = -1
+    let pointer = 0
 
     nodes.forEach((node) => {
         let walker = walkTree(node)
@@ -70,7 +71,7 @@ function buildPayloadMessage() {
             let node = res.value
             if (node.data) {
                 pointer += node.length
-            } else if (node.classList.contains('ace_line')) { // count 1 position - similar to \n
+            } else if (node.classList.contains('cm-line')) { // count 1 position - similar to \n
                 pointer += 1
             } else {
                 const nodeLength = countNodeLength(node)
@@ -85,7 +86,6 @@ function buildPayloadMessage() {
             // console.log(node);
         }
     })
-    // function submit() {\n    const pluginMessage
 
     console.table(output);
 
@@ -95,7 +95,7 @@ function buildPayloadMessage() {
 function submit() {
     const pluginMessage = {
         type: 'APPLY_THEME',
-        code: aceEditor.value.aceEditor.getValue(),
+        code: editorRef.value.code,
         code_highlighted: buildPayloadMessage()
     }
 
