f986a14d2aaebc81575878d1b1197572507b54e5
Tiago Matos <tiago@tiagomatos.com>
2023-02-10 10:15:30 +1100

    Add monaco

--- src/App.vue
+++ src/App.vue
@@ -51,6 +51,12 @@
                 class="inline-flex items-center rounded border border-transparent bg-indigo-600 px-2.5 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >Run
             </button>
+
+            <img
+                src="./assets/vue.svg"
+                class="w-10"
+                alt=""
+            >
         </div>
 
         <CodeEditor ref="codeEditor" />
