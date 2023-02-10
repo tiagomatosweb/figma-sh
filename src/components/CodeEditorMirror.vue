780fd1462464874ab633ed3c5e35bbf0b9d544ac
Tiago Matos <tiago@tiagomatos.com>
2023-02-10 17:14:25 +1100

    Add codemirror

--- src/components/CodeEditorMirror.vue
+++ src/components/CodeEditorMirror.vue
@@ -2,7 +2,7 @@
     <codemirror
         v-model="code"
         placeholder="Code goes here..."
-        :style="{ height: '400px' }"
+        :style="{ height: '400px', width: '300px' }"
         :autofocus="true"
         :indent-with-tab="true"
         :tab-size="2"
@@ -14,7 +14,7 @@
 </template>
 
 <script>
-import { computed, defineComponent, onMounted, ref, shallowRef } from 'vue'
+import { computed, defineComponent, onMounted, ref, shallowRef, watch } from 'vue'
 import { Codemirror } from 'vue-codemirror'
 import { javascript } from '@codemirror/lang-javascript'
 import { getHighlighter, setCDN } from 'shiki';
@@ -24,33 +24,39 @@ export default defineComponent({
         Codemirror
     },
     setup() {
-        const shiki = ref(null)
+        let shiki = null
         onMounted(async () => {
             setCDN('https://figma-sh.vercel.app/')
             // setCDN('http://127.0.0.1:5173/')
-            shiki.value = await getHighlighter({
-                theme: 'material-theme',
+            shiki = await getHighlighter({
+                theme: 'material-palenight',
                 lang: 'javascript',
             })
         });
 
-        const html = computed(() => {
-            console.log(shiki.value);
-            if (!shiki.value) { return; }
-            return shiki.value.codeToHtml(`${code.value}`, {
-                theme: 'material-theme',
+        const html = ref()
+        const code = ref(`console.log('Hello, world!')`)
+        watch(code, () => {
+            html.value = shiki.codeToHtml(`${code.value}`, {
+                theme: 'material-palenight',
                 lang: 'javascript',
-            });
+            }) ;
         })
 
+        // const html = computed(() => {
+        //     console.log(shiki);
+        //     if (!shiki) { return; }
+        //     return shiki.codeToHtml(`${code.value}`) ;
+        // })
 
-        const code = ref(`console.log('Hello, world!')`)
+
         const extensions = [javascript()]
 
         // Codemirror EditorView instance ref
         const view = shallowRef()
         const handleReady = (payload) => {
             view.value = payload.view
+            console.log(view.value);
         }
 
         // Status is available at all times via Codemirror EditorView
